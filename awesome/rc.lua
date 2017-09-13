23a9ef80f73f3712bafd5003cb89f368f9dfcfc9
Yurii Rashkovskii <yrashk@gmail.com>
2017-09-13 00:23:41 -0700

    Problem: awesome's config was using synchronous io.open
    Solution: use awesome.spawn instead

--- awesome/rc.lua
+++ awesome/rc.lua
@@ -566,6 +566,6 @@ client.connect_signal("focus", function(c) c.border_color = beautiful.border_foc
 client.connect_signal("unfocus", function(c) c.border_color = beautiful.border_normal end)
 
 -- Arrange the displays
-io.popen("xrandr --output eDP1 --left-of DP2-1 --mode 2048x1152")
+awesome.spawn("xrandr --output eDP1 --left-of DP2-1 --mode 2048x1152")
 
 -- }}}
