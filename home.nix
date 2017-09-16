05297b89b61145167cb7db20a078f29c4ad85bae
Yurii Rashkovskii <yrashk@gmail.com>
2017-09-16 12:44:39 -0700

    Problem: I need a good tool for viewing documents
    Solution: use Zathura

--- home.nix
+++ home.nix
@@ -8,6 +8,7 @@ with import <nixpkgs> {};
     pkgs.rlwrap
     pkgs.xorg.xdpyinfo # awesome/foggy seems to want it
     pkgs.vlc
+    pkgs.zathura # document viewer
     pkgs.htop
     pkgs.ncdu # Disk space usage analyzer
     pkgs.ripgrep # rg, fast grepper
