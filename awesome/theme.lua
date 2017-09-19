1cc15256744bd77f7dcc88d087af0a3375ac17fc
Yurii Rashkovskii <yrashk@gmail.com>
2017-09-19 15:30:50 -0700

    Problem: focused window title and tasklist merge
    Focused window title and tasklist have the same
    color (orange) and when they touch each other,
    they looks connected, which makes it looks weird
    (as if the titlebar has grown or something)
    
    Solution: make focused tasklist item have a white
    background

--- awesome/theme.lua
+++ awesome/theme.lua
@@ -27,6 +27,9 @@ theme.border_normal = "#000000"
 theme.border_focus  = theme.bg_focus
 theme.border_marked = "#91231c"
 
+theme.tasklist_bg_focus = "#ffffff"
+theme.tasklist_fg_focus = "#000000"
+
 -- There are other variable sets
 -- overriding the default one when
 -- defined, the sets are:
